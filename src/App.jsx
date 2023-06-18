ce0d09a51c74715d35c1251b0ca7de6dc43bca6f
Juan Manuel Gimenez <juanmauel20@gmail.com>
2023-06-18 20:00:09 -0300

    cree footer y navar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
- // se genera logica --  const [count, setCount] = useState(0)
+  // se genera logica --  const [count, setCount] = useState(0)
 
   return (
-    <div></div>
-  )
+    <section>
+      <h1> LAS MEJORES RECETAS DE COCINA CASERA</h1>
+      <div>
+        Más de 5 recetas de cocina lleva subidas, Cocina Casera y Fácil. Y aquí,
+        quiero hacer una recopilación con las 30 recetas de cocina faciles y
+        rápidas de más éxito del mismo. Aquellas recetas que no llevan mucho
+        trabajo, se hacen en un tiempo inferior a una hora y que por supuesto,
+        lo más importante…. ¡están deliciosas! La mayoría de ellas, están
+        enlazadas a su correspondiente vídeo, subidos al canal de Youtube ¡Que
+        viva la cocina!
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
