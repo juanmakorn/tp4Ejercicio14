edc74e30eb18c374e0c283220eeb22fc91eaaf2c
Juan Manuel Gimenez <juanmauel20@gmail.com>
2023-06-19 12:46:21 -0300

    se agrego correctamente el nav y footer

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 
 ### Consigna a cumplir 📚
 
-- Proyecto: TP React - Ejercicio 14
+- Proyecto: TP Reactjs- Ejercicio 14
 
 - Realizar el ejercicios del tp de react del 14. (Este ejercicio se puede realizar de forma individual o en grupo hasta 3 personas) subir link de github,  su respectivo readme (aclarar los integrantes de equipo si trabajan en grupo), deployar el proyecto en netlify u otro servidor.
 - NOTA: cada integrante de los grupos debe subir los links de su tarea. 
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from 'react';
+
+const Titulo = memo(() => {
+    return (
+        <div>
+            <h1> LAS MEJORES RECETAS DE COCINA CASERA</h1>
+       
+          Más recetas de cocina lleva subidas, Cocina Casera y Fácil. Y aquí,
+          quiero hacer una recopilación con recetas de cocina faciles y
+          rápidas de más éxito del mismo. Aquellas recetas que no llevan mucho
+          trabajo, se hacen en un tiempo inferior a una hora y que por supuesto,
+          lo más importante…. ¡están deliciosas!¡Que
+          viva la cocina!
+        </div>
+    );
+});
+
+export default Titulo;
