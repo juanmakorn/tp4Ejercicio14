521a81fe233185a729c93700825ddf5f8d587669
rafaortega191 <rafaelortega1911@gmail.com>
2023-06-19 17:29:00 -0300

    localstorage agregado
    uso de localstorage para almacenar las recetas agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PaginaPrincipal from "./components/views/PaginaPrincipal";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Titulos from "./components/Titulos";
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
 import RecipePage from "./components/RecipePage";
+import PaginaPrincipal from "./components/views/PaginaPrincipal";
 import PaginaAdministrador from "./components/views/PaginaAdministrador";
+import Error404 from "./components/views/Error404";
 
 function App() {
   // se genera logica --  const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<PaginaPrincipal></PaginaPrincipal>}></Route>
         <Route exact path="/administrador" element={<PaginaAdministrador></PaginaAdministrador>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
     </BrowserRouter>
   );
