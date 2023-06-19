9b661f2643da435dd364ae2bc1ac9f2d6c780faf
rafaortega191 <rafaelortega1911@gmail.com>
2023-06-19 12:20:38 -0300

    cambios en app
    cambios visuales en app

---
+++ src/components/Titulos.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Titulos = () => {
+    return (
+        <section>
+      <h1> LAS MEJORES RECETAS DE COCINA CASERA</h1>
+      <div>
+        Más de 5 recetas de cocina lleva subidas, Cocina Casera y Fácil. Y aquí,
+        quiero hacer una recopilación con las 30 recetas de cocina faciles y
+        rápidas de más éxito del mismo. Aquellas recetas que no llevan mucho
+        trabajo, se hacen en un tiempo inferior a una hora y que por supuesto,
+        lo más importante…. ¡están deliciosas! La mayoría de ellas, están
+        enlazadas a su correspondiente vídeo, subidos al canal de Youtube ¡Que
+        viva la cocina!
+      </div>
+    </section>
+    );
+};
+
+export default Titulos;
